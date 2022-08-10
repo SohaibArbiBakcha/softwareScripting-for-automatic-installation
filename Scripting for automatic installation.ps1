304c7a7fbf5a6b1e8f987bee4935e288353c546b
SohaibArbiBakcha <sohaibbakcha@gmail.com>
2022-08-10 16:01:48 +0100

    modify comments

--- Scripting for automatic installation.ps1
+++ Scripting for automatic installation.ps1
@@ -7,16 +7,16 @@ if (!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
 else{
     Write-Host "Chocolatey already exist !" -ForegroundColor Green
 }
-# list of program want to install
+# list of software want to install
 $Packages = 'googlechrome', 'anydesk.install', 'microsoft-teams', 'office365business','foxitreader','winrar'
-# execute command 'choco install <Packages> -y' for each program
+# execute command 'choco install <software> -y' for each software
 # -y : its for skip steps for
 ForEach ($PackageName in $Packages)
 {
     choco install $PackageName -y
 }
 
-#All software is installed
+#show list of software is installed
 choco list --localonly
 
 Write-Host "Chocolatey script finiched" -ForegroundColor Green
