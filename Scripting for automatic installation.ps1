3841e367efd9d28fa2c42b2e322fc35e11f07cfb
BAKCHA Sohaib Arbi <51241996+SohaibArbiBakcha@users.noreply.github.com>
2023-08-10 12:13:56 +0100

    mofify comments and softwares array

--- Scripting for automatic installation.ps1
+++ Scripting for automatic installation.ps1
@@ -1,23 +1,23 @@
 # to active external execution
 Set-ExecutionPolicy Bypass -Scope Process -Force
-# install chocolatey if not installed
+# install chocolatey if it's not installed
 if (!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
   Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 else{
     Write-Host "Chocolatey already exist !" -ForegroundColor Green
 }
-# list of software want to install
-$Packages = 'googlechrome', 'anydesk.install', 'microsoft-teams', 'office365business','foxitreader','winrar'
+# list of softwares want to install
+$Packages = 'brave', 'discord', 'steam', 'epicgameslauncher','7zip','ea-app' , 'vscode' , 'spotify' , 'nvidia-geforce-now' , 'vlc'
 # execute command 'choco install <software> -y' for each software
-# -y : its for skip steps for
+# -y : to skip virefication steps
 ForEach ($PackageName in $Packages)
 {
     choco install $PackageName -y
 }
 
-#show list of software is installed
+#show list of softwares installed
 choco list --localonly
 
-Write-Host "Chocolatey script finiched" -ForegroundColor Green
+Write-Host "Chocolatey script end" -ForegroundColor Green
 Pause
